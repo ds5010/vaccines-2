345e09db92a1c035d381b6362609a84bbb52d71c
Dilshath Shaik <dillu@Dilshaths-MacBook-Air.local>
2022-10-19 16:10:10 -0400

    Changes to animation python file

--- src/animation.py
+++ src/animation.py
@@ -6,8 +6,7 @@
 
 import imageio as iio
 import os
-import glob  #changes from meghDev
-import time #changes from meghDev
+from datetime import datetime
 
 def create_gif(filename_save):
     """This function creates a gif animation from a folder of png images
@@ -16,29 +15,13 @@ def create_gif(filename_save):
     """
     #makes a list of im NumPy arrays based on a list of .png images (read from folder)
     images = list()
-
-    #Changes from meghDev
-    dir_name = 'img/'
-    list_of_files = filter( os.path.isfile,
-                        glob.glob(dir_name + '*') )
-    
-    list_of_files = sorted( list_of_files,
-                        key = os.path.getmtime) 
-    #to here
-    
     #this part looks at the img directory and reads in all the files that end with .png (only going to bring in those)
-<<<<<<< HEAD
     for filename in sorted(os.listdir('img')):
         if filename[-8:] == '2021.png' and not filename == 'comparison.png':
-=======
-    for filename in list_of_files:  #changes from meghDev        #sorted(os.listdir('img')):
-        if filename[-4:] == '.png' and not filename == 'comparison.png':
->>>>>>> main
             f = os.path.join('img',filename)
             im = iio.imread(f)
             images.append(im)
     images.append(iio.imread(os.path.join('img','08-31-2022.png')))
-
     #making the gif from the pngs
     iio.mimsave(filename_save,images,duration = 1)
 
